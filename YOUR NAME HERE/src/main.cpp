0bf9bc95750c0ba30b29d3e7c9141a17878e2618
NobleOMD1 <nobleomd1@outlook.com>
2022-01-21 10:04:48 +0000

    Got scaling fixed.

--- YOUR NAME HERE/src/main.cpp
+++ YOUR NAME HERE/src/main.cpp
@@ -7,10 +7,15 @@ int main()
 {
 	// Initialization
 	//--------------------------------------------------------------------------------------
-	raylib::Window window(settings::screenSize.x, settings::screenSize.y, settings::title);
+	raylib::Window window(
+		settings::screenSize.x * settings::scaleFactor,
+		settings::screenSize.y * settings::scaleFactor,
+		settings::title
+	);
 
 	window.SetState(FLAG_VSYNC_HINT); // Use V-Sync to autodetect and run at monitor refresh rate
 
+	raylib::RenderTexture2D scalerCanvas{(int) settings::screenSize.x, (int) settings::screenSize.y};
 	PlayerCharacter player{ { 1, 2 }, { 25, 15 }, { 128, 68 } }; // Initialise a player with texture
 	//--------------------------------------------------------------------------------------
 
@@ -31,10 +36,24 @@ int main()
 
 		// Draw
 		//----------------------------------------------------------------------------------
-		window.BeginDrawing();
+		scalerCanvas.BeginMode();
 		{
 			window.ClearBackground(settings::backgroundColour);
 			player.draw();
+		}
+		scalerCanvas.EndMode();
+
+		window.BeginDrawing();
+		{
+			window.ClearBackground(WHITE);
+			DrawTexturePro(
+				scalerCanvas.texture,														// Texture
+				raylib::Rectangle(0, 0, settings::screenSize.x, -settings::screenSize.y),	// Source
+				raylib::Rectangle({0, 0}, settings::screenSize * settings::scaleFactor),	// Destination
+				raylib::Vector2(0, 0),														// Origin
+				0,																			// Rotation
+				WHITE																		// Tint
+			);
 		}
 		window.EndDrawing();
 		//----------------------------------------------------------------------------------
--- YOUR NAME HERE/src/settings.hpp
+++ YOUR NAME HERE/src/settings.hpp
@@ -10,6 +10,7 @@ namespace settings
 	inline int tileSize = 16; // Tile size used to multiply many calculations, in future dynamically change to percentage of screen size
 	inline raylib::Vector2 gridSize{ 50, 30 }; // Size of the screen "grid" in tiles
 	inline raylib::Vector2 screenSize = gridSize * tileSize; // True screen size
+	inline int scaleFactor = 2;
 
 	inline raylib::Color backgroundColour(0x282c34ff); // Background colour, note the required use of 100% opacity
 
