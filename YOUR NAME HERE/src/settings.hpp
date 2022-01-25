ca4e0113d51e97807c3588a64959d8fd348dda7f
NobleOMD1 <nobleomd1@outlook.com>
2022-01-25 00:48:09 +0000

    Changed scale factor to 3x and grid size is now 38x20

--- YOUR NAME HERE/src/settings.hpp
+++ YOUR NAME HERE/src/settings.hpp
@@ -6,14 +6,14 @@ namespace settings
 	// Window Settings
 	inline std::string title = "YOUR NAME HERE"; // Title of the window
 
-	inline int tileSize = 16; // Tile size used to multiply many calculations, in future dynamically change to percentage of screen size
-	inline raylib::Vector2 gridSize{50, 28}; // Size of the screen "grid" in tiles
+	inline constexpr int tileSize = 16; // Tile size used to multiply many calculations, in future dynamically change to percentage of screen size
+	inline raylib::Vector2 gridSize{38, 20}; // Size of the screen "grid" in tiles
 	inline raylib::Vector2 screenSize = gridSize * tileSize; // True screen size
 
 	inline raylib::Color backgroundColour(0x282c34ff); // Background colour, note the required use of 100% opacity
 
 	// Scaling settings
-	inline int scaleFactor = 2; // Default scale factor
+	inline int scaleFactor = 3; // Default scale factor
 	inline raylib::Vector2 scaleOrigin = {0, 0}; // Origin used by the scaleCanvas
 
 	// Get the current screen size according to the scale factor
