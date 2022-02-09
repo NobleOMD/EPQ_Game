2b3ba8e021eb4da8483111ecaaa6fa8d8b8f392c
NobleOMD1 <nobleomd1@outlook.com>
2022-02-09 15:23:40 +0000

    Fixed issues from merging branches

---
+++ YOUR NAME HERE/src/gameObject.cpp
@@ -0,0 +1,153 @@
+#include "gameObject.hpp"
+
+// GameObject base class
+//---------------------------------------------------------------------------------
+
+// Construction
+
+GameObject::GameObject(
+	raylib::Vector2 size,
+	raylib::Vector2 gridPos,
+	raylib::Texture *texture,
+	raylib::Rectangle textureRect,
+	raylib::Color colour
+)
+	:
+	size(size),
+	gridPosition(gridPos),
+	objectTexture(texture),
+	textureRect(textureRect),
+	objectColour(colour) {
+	allObjects.push_back(this);
+}
+
+// Updates
+
+void GameObject::update() {}
+
+raylib::Vector2 GameObject::getObjectPosition() {
+	return gridPosition * settings::tileSize;
+}
+
+
+// Drawing
+
+void GameObject::drawFilled() {
+	objectRectangle.Draw(objectColour);
+}
+
+
+void GameObject::drawOutline(int thickness) {
+	objectRectangle.DrawLines(objectColour, thickness);
+}
+
+void GameObject::drawTexture() {
+	// Janky texture offset that is used to align texture with collison box
+	raylib::Vector2 textureOffset = (raylib::Vector2) objectRectangle.GetSize() - (raylib::Vector2) textureRect.GetSize();
+
+	objectTexture->Draw(
+		textureRect,
+		getObjectPosition() + raylib::Vector2(textureOffset.x / 2, textureOffset.y) // Move the texture inline with the collision box
+	);
+}
+
+void GameObject::draw() {
+	drawTexture();
+}
+
+// CollisionObject derived GameObject class
+//---------------------------------------------------------------------------------
+
+CollisionObject::CollisionObject(raylib::Vector2 size, raylib::Vector2 gridPos, raylib::Texture *texture, raylib::Rectangle textureRect, float health, raylib::Color colour)
+	:
+	GameObject(size, gridPos, texture, textureRect, colour),
+	health(health)
+{
+	collisionObjects.push_back(this);
+}
+
+CollisionObject *CollisionObject::collisionCheck(std::vector<CollisionObject *> objects) {
+	// Check for collision within the 
+	for (CollisionObject *object : objects) {
+		if (this == object) continue; // If the object is ourself continue.
+
+		// If this objects hit box is colliding with another undo the translation
+		if (CheckCollisionRecs(this->objectRectangle, object->objectRectangle)) return object;
+	}
+
+	return nullptr; // If no collision is found return a nullptr
+}
+
+// Entity derived GameObject class
+//---------------------------------------------------------------------------------
+
+void Entity::move(raylib::Vector2 translation) {
+	gridPosition += translation; // Move by translation vector
+	objectRectangle.SetPosition(getObjectPosition()); // Update the onscreen position
+
+	if (collisionCheck(collisionObjects) != nullptr) gridPosition -= translation;
+
+	game::clampWithin(settings::gridSize, gridPosition, size); // Clamp within the overall grid
+	objectRectangle.SetPosition(getObjectPosition()); // Update the onscreen position
+}
+
+
+// PlayerCharacter derived Entity class
+//---------------------------------------------------------------------------------
+
+void PlayerCharacter::update() {
+	if (health < 0.0F) return;
+
+	// Player keyboard input to move
+	if (IsKeyPressed(KEY_W)) Entity::move({0, -1});
+	if (IsKeyPressed(KEY_A)) Entity::move({-1, 0});
+	if (IsKeyPressed(KEY_S)) Entity::move({0, 1});
+	if (IsKeyPressed(KEY_D)) Entity::move({1, 0});
+}
+
+// Enemy derived Entity class
+//---------------------------------------------------------------------------------
+
+void Enemy::update() {
+	// Decrement the timer by frame time
+	if (moveTimer >= 0.0f) {
+		moveTimer -= GetFrameTime();
+		return;
+	}
+
+	moveTimer = game::mt19937range(speed - speedRange, speed + speedRange); // Reset the movement timer
+
+	// Move direction vector is faster than switch case
+	std::vector<raylib::Vector2> moveDirection{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+	move(moveDirection[game::random() % 4]);
+}
+
+// Wall derived GameObject class
+//---------------------------------------------------------------------------------
+
+Wall::Wall(raylib::Vector2 gridPos)
+	:
+	CollisionObject({1, 1}, gridPos, nullptr, {0, 0, 0, 0}, FLT_MAX, {0, 0, 0, 0}) {
+	collisionObjects.push_back(this);
+}
+
+void Wall::draw() {
+	drawFilled();
+}
+
+// DamageEntity derived GameObject class
+//---------------------------------------------------------------------------------
+
+void DamageEntity::move(raylib::Vector2 translation) {
+	gridPosition += translation; // Move by translation vector
+	game::clampWithin(settings::gridSize, gridPosition, size); // Clamp within the overall grid
+	objectRectangle.SetPosition(getObjectPosition()); // Update the onscreen position
+}
+
+void DamageEntity::update() {
+	CollisionObject *hitObject = collisionCheck(collisionObjects);
+	if (hitObject == nullptr) return;
+
+	hitObject->health -= damage;
+}
---
+++ YOUR NAME HERE/src/objects.cpp
@@ -0,0 +1,136 @@
+#include "objects.hpp"
+
+// ObjectRectangle: base class
+//---------------------------------------------------------------------------------
+ObjectRectangle::ObjectRectangle(raylib::Vector2 size, raylib::Vector2 position)
+	:
+	raylib::Rectangle(position *settings::tileSize, size *settings::tileSize),
+	size(size),
+	position(position) {
+	allObjects.push_back(this);
+}
+
+void ObjectRectangle::update() {}
+
+void ObjectRectangle::draw() {}
+
+void ObjectRectangle::debug() {
+	// Draw object outline
+	::DrawRectangleLines(static_cast<int>(x), static_cast<int>(y), static_cast<int>(width), static_cast<int>(height), BLUE);
+}
+
+void ObjectRectangle::updatePosition() {
+	x = position.x * settings::tileSize;
+	y = position.y * settings::tileSize;
+
+	width = size.x * settings::tileSize;
+	height = size.y * settings::tileSize;
+}
+//---------------------------------------------------------------------------------
+
+
+// ObjectTexture: ObjectRectangle drawn with texture
+//---------------------------------------------------------------------------------
+ObjectTexture::ObjectTexture(raylib::Vector2 size, raylib::Vector2 position, raylib::Texture *texture, raylib::Rectangle textureRect)
+	:
+	ObjectRectangle(size, position),
+	objectTexture(texture),
+	textureRect(textureRect) {
+}
+
+void ObjectTexture::draw() {
+		// Janky texture offset that is used to align texture with collison box
+	raylib::Vector2 textureOffset = (raylib::Vector2) this->GetSize() - (raylib::Vector2) textureRect.GetSize();
+
+	objectTexture->Draw(
+		textureRect,
+		(raylib::Vector2) this->GetPosition() + raylib::Vector2(textureOffset.x / 2, textureOffset.y) // Move the texture inline with the collision box
+	);
+}
+//---------------------------------------------------------------------------------
+
+
+// Collision: adds collision to ObjectRectangle
+//---------------------------------------------------------------------------------
+ObjectRectangle *collisionCheck(ObjectRectangle *target, std::vector<ObjectRectangle *> objects) {
+	// Check for collision within a list of objects with collison
+	for (ObjectRectangle *object : objects) {
+		if (target == object) continue; // If the object is ourself continue.
+
+		// If this objects hit box is colliding with another undo the translation
+		if (CheckCollisionRecs(*target, *object)) return object;
+	}
+
+	return nullptr; // If no collision is found return a nullptr
+}
+//---------------------------------------------------------------------------------
+
+
+// Entity: combines Textures with Collision and adds movement
+//---------------------------------------------------------------------------------
+void movement(ObjectRectangle *target, raylib::Vector2 translation) {
+	target->position += translation; // Move by translation vector
+	game::clampWithin(settings::gridSize, target->position, target->size); // Clamp within the overall grid
+	target->updatePosition(); // Update the onscreen position
+}
+//---------------------------------------------------------------------------------
+
+
+// Entity: combines Textures, Collision and Movement
+//---------------------------------------------------------------------------------
+Entity::Entity(raylib::Vector2 size, raylib::Vector2 position, raylib::Texture *texture, raylib::Rectangle textureRect)
+	:
+	ObjectTexture(size, position, texture, textureRect) {
+	collisionObjects.push_back(this);
+}
+
+void Entity::move(raylib::Vector2 translation) {
+	movement(this, translation);
+	if (collisionCheck(this, collisionObjects) != nullptr) movement(this, -translation);
+}
+//---------------------------------------------------------------------------------
+
+
+// PlayerCharacter: derived from Entity
+//---------------------------------------------------------------------------------
+void PlayerCharacter::update() {
+	// Player keyboard input to move
+	if (IsKeyPressed(KEY_W)) move({0, -1});
+	if (IsKeyPressed(KEY_A)) move({-1, 0});
+	if (IsKeyPressed(KEY_S)) move({0, 1});
+	if (IsKeyPressed(KEY_D)) move({1, 0});
+}
+//---------------------------------------------------------------------------------
+
+
+// Enemy: derived from Entity
+//---------------------------------------------------------------------------------
+Enemy::Enemy(raylib::Vector2 size, raylib::Vector2 position, raylib::Texture *texture, raylib::Rectangle textureRect)
+	:
+	Entity(size, position, texture, textureRect) {
+	enemyObjects.push_back(this);
+}
+
+void Enemy::update() {
+	// Decrement the timer by frame time
+	if (moveTimer >= 0.0f) {
+		moveTimer -= GetFrameTime();
+		return;
+	}
+
+	moveTimer = game::mt19937range(speed - speedRange, speed + speedRange); // Reset the movement timer
+
+	// Move direction vector is faster than switch case
+	std::vector<raylib::Vector2> moveDirection{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+	move(moveDirection[game::random() % 4]);
+}
+
+// Wall: collision at grid position
+//---------------------------------------------------------------------------------
+Wall::Wall(raylib::Vector2 gridPos)
+	:
+	ObjectRectangle({1, 1}, gridPos) {
+	collisionObjects.push_back(this);
+}
+
